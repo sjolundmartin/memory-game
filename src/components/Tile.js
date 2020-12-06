0edbc2e6263b1d924559590dad1e5b86110052d6
sjolundmartin <martinsjolund@hotmail.com>
2020-12-06 20:34:19 +0100

    added placeholder for tile colors

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
+const colors = {
+  hidden: 'lightgray',
+  matched: 'lightgreen',
+  wrong: 'lightcoral',
+  turned: 'deepskyblue',
+};
+
 const Tile = (props) => {
   return (
     <>
       <button
         className="tile"
+        style={{ backgroundColor: colors['hidden'] }}
         onClick={() => console.log(props.value)}
       ></button>
     </>
