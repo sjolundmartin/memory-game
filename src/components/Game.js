0807e5ad1943be009e8055783d8fc12fad4ccba9
sjolundmartin <martinsjolund@hotmail.com>
2020-12-07 13:38:37 +0100

    candidate state now updates when tile is pressed

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,41 +1,65 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import utils from '../utils';
 
 import Tile from './Tile';
 
 const Game = (props) => {
   const [matchedTiles, setMatchedTiles] = useState([1, 4]);
-  const [candidateTiles, setCandidateTiles] = useState([6, 10]);
-  //   const isAMatch = (pair) => {
-  //     return pair[0] === pair[1];
-  //   };
+  const [candidateTiles, setCandidateTiles] = useState({
+    keys: [200],
+    values: ['test'],
+  });
 
-  const onTileClick = (key) => {
-    if (matchedTiles.includes(key) || candidateTiles.includes(key)) {
+  const onTileClick = (key, value) => {
+    if (matchedTiles.includes(key) || candidateTiles.keys.includes(key)) {
+      console.log('already clicked');
       return;
     }
-
-    if (candidateTiles.length() <= 1) {
-      return;
+    if (candidateTiles.keys.length <= 1) {
+      setCandidateTiles((prevState) => ({
+        keys: [...prevState.keys, key],
+        values: [...prevState.values, value],
+      }));
     }
   };
+
+  //   useEffect(() => {
+  //     if (candidateTiles.keys.lengt == 2) {
+  //       if (candidateTiles.values[0] == candidateTiles.values[1]) {
+  //         console.log('matched');
+  //       }
+  //     }
+  //   }, [candidateTiles]);
+
+  //   const checkIfMatch = () => {
+  //     if (candidateTiles.keys.length == 2) {
+  //       if (candidateTiles[0] === candidateTiles[1]) {
+  //         console.log('matched');
+  //         this.setState((prevState) => ({
+  //           matchedTiles: [...prevState.matchedTiles, ...candidateTiles],
+  //         }));
+  //       }
+  //     }
+  //   };
 
   const tileStatus = (tile) => {
     if (matchedTiles.includes(tile)) {
       return 'matched';
     }
-
-    if (candidateTiles.includes(tile)) {
+    if (candidateTiles.keys.includes(tile)) {
       return 'candidate';
     }
-
+    // if (candidateTiles.keys.length == 2) {
+    //   //kolla om match och lägg till i matched eller returnera 'wrong'
+    // }
     return 'hidden';
   };
 
   const createTileArray = () => {
     if (props.pairType == 'string') {
       return utils.createStringPairs(props.noOfPairs);
-    } else if (props.pairType == 'num') {
+    }
+    if (props.pairType == 'num') {
       return utils.createNumPairs(props.noOfPairs);
     }
   };
@@ -48,6 +72,7 @@ const Game = (props) => {
           {createTileArray().map((value, index) => (
             <Tile
               key={index}
+              index={index}
               value={value}
               onClick={onTileClick}
               status={tileStatus(index)}
