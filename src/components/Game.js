14e07f2e181940e52313067406abcc41eb8e1463
sjolundmartin <martinsjolund@hotmail.com>
2020-12-08 11:17:06 +0100

    extracted hooks to a custom hook

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,33 +1,17 @@
-import React, { useState, useEffect, useRef } from 'react';
-//import tileArrayMaker from '../tileArrayMaker';
-import utils from '../utils';
+import React from 'react';
+import useGameState from '../useGameState';
 
 import Tile from './Tile';
 
-//Based on the type of game passed by the parent, fetches an array of tile values from ../utils.
-const createTileArray = (type, noOfPairs) => {
-  if (type == 'string') {
-    return utils.createStringPairs(noOfPairs);
-  }
-  if (type == 'num') {
-    return utils.createNumPairs(noOfPairs);
-  }
-  //All tiles have the same value
-  if (type == 'easy') {
-    return utils.createEasyMode(noOfPairs);
-  }
-};
-
 const Game = (props) => {
-  //STATES
-  const [matchedTiles, setMatchedTiles] = useState([]);
-  const [candidateKeys, setCandidateKeys] = useState([]);
-  const [candidateValues, setCandidateValues] = useState([]);
-  const [wrongPair, setWrongPair] = useState([]);
-  const tiles = useRef([...createTileArray(props.type, props.noOfPairs)]);
-
-  //Returns the comparison of two candidateValues as Boolean
-  const isAMatch = candidateValues[0] === candidateValues[1];
+  const {
+    matchedTiles,
+    candidateKeys,
+    candidateValues,
+    wrongPair,
+    tiles,
+    setGameState,
+  } = useGameState(props.type, props.noOfPairs);
 
   //Logic when a tile in the UI is clicked
   const onTileClick = (key, value, status) => {
@@ -38,29 +22,8 @@ const Game = (props) => {
     const newCandidateKeys = candidateKeys.concat([key]);
     const newCandidateValues = candidateValues.concat([value]);
 
-    if (candidateKeys.length <= 1) {
-      setCandidateKeys(newCandidateKeys);
-      setCandidateValues(newCandidateValues);
-    }
+    setGameState(newCandidateKeys, newCandidateValues);
   };
-
-  //Checks if there are two tiles selected and puts them in matchedTiles if they match. Then clears candidateValues and candidateKeys.
-  //The function runs when onTileClick is finished.
-  useEffect(() => {
-    if (candidateKeys.length == 2) {
-      if (isAMatch) {
-        const newMatchedTiles = matchedTiles.concat(candidateKeys);
-        setMatchedTiles(newMatchedTiles);
-      } else {
-        setWrongPair(candidateKeys);
-        setTimeout(() => {
-          setWrongPair([]);
-        }, 1000);
-      }
-      setCandidateKeys([]);
-      setCandidateValues([]);
-    }
-  });
 
   //Returns the status used by the Tile component.
   const tileStatus = (tileKey) => {
---
+++ src/useGameState.js
@@ -0,0 +1,63 @@
+import { useState, useEffect, useRef } from 'react';
+import utils from './utils';
+
+const createTileArray = (type, noOfPairs) => {
+  if (type == 'string') {
+    return utils.createStringPairs(noOfPairs);
+  }
+  if (type == 'num') {
+    return utils.createNumPairs(noOfPairs);
+  }
+  //All tiles have the same value
+  if (type == 'easy') {
+    return utils.createEasyMode(noOfPairs);
+  }
+};
+
+const useGameState = (type, noOfPairs) => {
+  //STATES
+  const [matchedTiles, setMatchedTiles] = useState([]);
+  const [candidateKeys, setCandidateKeys] = useState([]);
+  const [candidateValues, setCandidateValues] = useState([]);
+  const [wrongPair, setWrongPair] = useState([]);
+  const tiles = useRef([...createTileArray(type, noOfPairs)]);
+
+  //Checks if there are two tiles selected and puts them in matchedTiles if they match. Then clears candidateValues and candidateKeys.
+  //The function runs when onTileClick is finished.
+  useEffect(() => {
+    if (candidateKeys.length == 2) {
+      if (isAMatch) {
+        const newMatchedTiles = matchedTiles.concat(candidateKeys);
+        setMatchedTiles(newMatchedTiles);
+      } else {
+        setWrongPair(candidateKeys);
+        setTimeout(() => {
+          setWrongPair([]);
+        }, 1000);
+      }
+      setCandidateKeys([]);
+      setCandidateValues([]);
+    }
+  });
+
+  //Returns the comparison of two candidateValues as Boolean
+  const isAMatch = candidateValues[0] === candidateValues[1];
+
+  const setGameState = (newCandidateKeys, newCandidateValues) => {
+    if (candidateKeys.length <= 1) {
+      setCandidateKeys(newCandidateKeys);
+      setCandidateValues(newCandidateValues);
+    }
+  };
+
+  return {
+    matchedTiles,
+    candidateKeys,
+    candidateValues,
+    wrongPair,
+    tiles,
+    setGameState,
+  };
+};
+
+export default useGameState;
